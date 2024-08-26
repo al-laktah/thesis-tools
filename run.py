21da63484b75d7d410c568d01655fbbc36342fef
Mohammed Al-Laktah <mohammed@al-laktah.com>
2024-08-26 22:28:36 +0200

    changed save location for logs and generated files

--- run.py
+++ run.py
@@ -14,12 +14,12 @@ data = Ris.get_rev_reports(session)
 unique_id, responses, log = generate_from(7, 4, session, data)
 
 # Write responses to a JSON file
-with open(f'{unique_id}_responses.json', 'w', encoding='utf-8') as responses_file:
+with open(f'data/generated/{unique_id}_responses.json', 'w', encoding='utf-8') as responses_file:
     json.dump(responses, responses_file, indent=4, ensure_ascii=False)
 
 # Write log to a JSON file
 if log:
-    with open(f'{unique_id}_log.json', 'w', encoding='utf-8') as log_file:
+    with open(f'data/logs/{unique_id}_log.json', 'w', encoding='utf-8') as log_file:
         json.dump(log, log_file, indent=4, ensure_ascii=False)
 
     print(f"Done! Check the files {unique_id}_responses.json and {unique_id}_log.json")
