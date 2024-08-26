bd2f1cc686741b9f464ad110465e6557ef7f0783
Mohammed Al-Laktah <mohammed@al-laktah.com>
2024-08-26 18:50:50 +0200

    change so log is only created if it is not empty

--- run.py
+++ run.py
@@ -11,15 +11,17 @@ session = Session()
 
 data = Ris.get_rev_reports(session)
 
-unique_id, responses, log = generate_from(1, 4, session, data)
+unique_id, responses, log = generate_from(5, 4, session, data)
 
 # Write responses to a JSON file
 with open(f'{unique_id}_responses.json', 'w', encoding='utf-8') as responses_file:
     json.dump(responses, responses_file, indent=4)
 
 # Write log to a JSON file
-with open(f'{unique_id}_log.json', 'w', encoding='utf-8') as log_file:
-    json.dump(log, log_file, indent=4, ensure_ascii=False)
+if log:
+    with open(f'{unique_id}_log.json', 'w', encoding='utf-8') as log_file:
+        json.dump(log, log_file, indent=4, ensure_ascii=False)
     
-    
-print(f"Done! Check the files {unique_id}_responses.json and {unique_id}_log.json")
+    print(f"Done! Check the files {unique_id}_responses.json and {unique_id}_log.json")
+else:
+    print(f"Done! Check the file {unique_id}_responses.json")
