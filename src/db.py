4eb21a68a713bc5569302435364f5ebf200f9c98
Mohammed Al-Laktah <mohammed@al-laktah.com>
2024-08-27 15:57:58 +0200

    db update

--- src/db.py
+++ src/db.py
@@ -2,11 +2,42 @@
 
 from typing import Dict, List
 import pandas as pd
-from sqlalchemy.orm import DeclarativeBase as Base
+from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, Session, mapped_column
 from sqlalchemy.dialects.sqlite import INTEGER, JSON, TEXT
 
 # from sqlalchemy import ForeignKey
+
+
+class Base(DeclarativeBase):
+    """Base class for all database models."""
+
+    __abstract__ = True
+    __table_args__ = {"extend_existing": True}
+
+    id: Mapped[int] = mapped_column(INTEGER, primary_key=True, nullable=False)
+
+    def to_dict(self):
+        """Convert an instance to a dictionary."""
+        return {
+            column.name: getattr(self, column.name) for column in self.__table__.columns
+        }
+
+    @classmethod
+    def get_by_id(cls, session: Session, x_id: int):
+        """Function to get a row by its id."""
+        return session.query(cls).filter_by(id=x_id).first()
+
+    @classmethod
+    def to_dataframe(cls, session: Session) -> pd.DataFrame:
+        """Convert the entire table to a pandas DataFrame."""
+        instances = session.query(cls).all()
+        if not instances:
+            return pd.DataFrame()
+
+        # Convert each instance to a dictionary
+        data = [instance.to_dict() for instance in instances]
+        return pd.DataFrame(data)
 
 
 class Models(Base):
@@ -14,7 +45,6 @@ class Models(Base):
 
     __tablename__ = "models"
 
-    id: Mapped[int] = mapped_column(INTEGER, primary_key=True, nullable=False)
     name: Mapped[str] = mapped_column(TEXT, nullable=False)
     size: Mapped[str] = mapped_column(TEXT, nullable=False)
     options: Mapped[Dict] = mapped_column(JSON)
@@ -24,38 +54,23 @@ class Models(Base):
             return f"{self.name}:{self.size}_o:{self.options}"
         return f"{self.name}:{self.size}_Default"
 
-    @classmethod
-    def get_by_id(cls, session: Session, model_id: int):
-        """Function to get a model by its id."""
-        return session.query(cls).filter_by(id=model_id).first()
-
 
 class Prompts(Base):
     """Model class for the prompts table."""
 
     __tablename__ = "prompts"
 
-    id: Mapped[int] = mapped_column(INTEGER, primary_key=True, nullable=False)
-
     prompt: Mapped[str] = mapped_column(TEXT, nullable=False)
-
     tags: Mapped[List[str]] = mapped_column(JSON)
 
     def __repr__(self) -> str:
         return f"prompt {self.id}: {self.prompt}"
-
-    @classmethod
-    def get_by_id(cls, session: Session, prompt_id: int):
-        """Function to get a prompt by its id."""
-        return session.query(cls).filter_by(id=prompt_id).first()
 
 
 class Ris(Base):
     """Model class for the ris table."""
 
     __tablename__ = "ris"
-
-    id: Mapped[int] = mapped_column(INTEGER, primary_key=True, nullable=False)
 
     revision_1: Mapped[str] = mapped_column(TEXT)
     revision_2: Mapped[str] = mapped_column(TEXT)
@@ -64,11 +79,6 @@ class Ris(Base):
 
     def __repr__(self) -> str:
         return f"Ris Report: {self.id}"
-
-    @classmethod
-    def get_by_id(cls, session: Session, ris_id: int):
-        """Function to get a ris report by its id."""
-        return session.query(cls).filter_by(id=ris_id).first()
 
     @classmethod
     def get_rev_reports(cls, session: Session):
