df9a09159027bc3aa369fc91b6043acebee02e46
Mohammed Al-Laktah <mohammed@al-laktah.com>
2024-08-27 15:33:19 +0200

    update to test more models

--- run.py
+++ run.py
@@ -13,19 +13,22 @@ session = Session()
 
 data = Ris.get_rev_reports(session)
 
-unique_id, responses, log = generate_from(7, 4, session, data)
+for model_id in [1, 5, 7, 8, 9, 12, 14]:
+    unique_id, responses, log = generate_from(model_id, 4, session, data)
 
-# Write responses to a JSON file
-with open(
-    f"data/generated/{unique_id}_responses.json", "w", encoding="utf-8"
-) as responses_file:
-    json.dump(responses, responses_file, indent=4, ensure_ascii=False)
+    # Write responses to a JSON file
+    with open(
+        f"data/generated/{unique_id}_responses.json", "w", encoding="utf-8"
+    ) as responses_file:
+        json.dump(responses, responses_file, indent=4, ensure_ascii=False)
 
-# Write log to a JSON file
-if log:
-    with open(f"data/logs/{unique_id}_log.json", "w", encoding="utf-8") as log_file:
-        json.dump(log, log_file, indent=4, ensure_ascii=False)
+    # Write log to a JSON file
+    if log:
+        with open(f"data/logs/{unique_id}_log.json", "w", encoding="utf-8") as log_file:
+            json.dump(log, log_file, indent=4, ensure_ascii=False)
 
-    print(f"Done! Check the files {unique_id}_responses.json and {unique_id}_log.json")
-else:
-    print(f"Done! Check the file {unique_id}_responses.json")
+        print(
+            f"Done! Check the files {unique_id}_responses.json and {unique_id}_log.json"
+        )
+    else:
+        print(f"Done! Check the file {unique_id}_responses.json")
