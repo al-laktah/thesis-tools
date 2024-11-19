f2a6814cd02d8b1efb15611ab7452f10b4f2fd37
Mohammed Al-Laktah <mohammed@al-laktah.com>
2024-11-19 05:23:08 +0100

    a

--- setup.py
+++ setup.py
@@ -9,10 +9,12 @@ setup(
     package_dir={"": "src"},
     install_requires=[
         "pandas>=2.2.2",
+        "numpy>=1.21.2",
         "ollama>=0.3.1",
         "SQLAlchemy>=2.0.32",
         "language_tool_python>=2.8.1",
         "diff_match_patch>=20230430"
-        "matplotlib>=3.9.2"
+        "matplotlib>=3.9.2",
+        "seaborn>=0.11.2",
     ],
 )
